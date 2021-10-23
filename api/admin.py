31f640e4bfd2b8b37cc9ef80fc0ab631243b5542
Jim Su <jimsu2012@gmail.com>
2021-10-23 17:03:32 -0400

    Add Habit, Article, Goal, and GoalDay to admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Habit, Article, Goal, GoalDay
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Habit)
+admin.site.register(Article)
+admin.site.register(Goal)
+admin.site.register(GoalDay)
