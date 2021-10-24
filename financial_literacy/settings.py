6ebfa72e0d4871de0f14f0f98b84112719edeff7
Jim Su <jimsu2012@gmail.com>
2021-10-24 11:30:54 -0400

    Hotfix: allow all CORS origins

--- financial_literacy/settings.py
+++ financial_literacy/settings.py
@@ -76,12 +76,13 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:8000'
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+#     'http://localhost:8000',
+#     'http://127.0.0.1:3000',
+#     'http://127.0.0.1:8000'
+# ]
 
 ROOT_URLCONF = 'financial_literacy.urls'
 
