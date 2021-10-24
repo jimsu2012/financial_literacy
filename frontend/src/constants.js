a8a4b52e747ff95601ed5c4e672c2d923108e790
Jim Su <jimsu2012@gmail.com>
2021-10-24 11:44:02 -0400

    Hotfix change axios baseUrl port

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://127.0.0.1:8000";
+const baseUrl = "http://127.0.0.1:8721";
 export const axiosInstance = axios.create({
   baseURL: baseUrl,
 });
